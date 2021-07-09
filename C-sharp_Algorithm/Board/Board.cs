3d11d5ec909b8f7e05791f2925fb2b0f22cd0ef7
dubious123 <dubious123@hanyang.ac.kr>
2021-07-09 10:27:30 +0900

    Maze_Generattion_Algorithm

--- C-sharp_Algorithm/Board/Board.cs
+++ C-sharp_Algorithm/Board/Board.cs
@@ -12,7 +12,7 @@ namespace Board
         const char CIRCLE = '\u25cf';
         public TileType[,] _tile;
         public int _ROW_SIZE, _COLUMN_SIZE;
-
+        Random rand = new Random();
         public enum TileType
         {
             //벽 : RED
@@ -22,15 +22,169 @@ namespace Board
         }
         public void Initialize(int Row,int Column)
         {
-            
+            //이 미로생성 알고리즘은 사이즈가 홀수여야함
+            if (Row % 2 == 0 || Column % 2 == 0)
+            {
+                return;
+            }
             _tile = new TileType[Row, Column];
             _ROW_SIZE = Row;
             _COLUMN_SIZE = Column;
-            for(int y = 0; y < _ROW_SIZE; y++)
+            GenerateBySideWinder();
+        }
+
+        private void GenerateBySideWinder()
+        {
+
             {
-                for(int x = 0; x < _COLUMN_SIZE; x++)
+                //길을 다 막아버리는 작업
+                for (int y = 0; y < _ROW_SIZE; y++)
                 {
-                    if (x == 0 || x == _COLUMN_SIZE - 1|| y == 0 || y == _ROW_SIZE - 1)
+                    for (int x = 0; x < _COLUMN_SIZE; x++)
+                    {
+                        if (x % 2 == 0 || y % 2 == 0)
+                        {
+                            _tile[y, x] = TileType.Wall;
+                        }
+                        else
+                        {
+                            _tile[y, x] = TileType.Empty;
+                        }
+                    }
+                }
+
+                //랜덤으로 우측 혹은 아래로 길을 둟는 작업
+                for (int y = 0; y < _ROW_SIZE; y++)
+                {
+                    int count = 1;
+                    for (int x = 0; x < _COLUMN_SIZE; x++)
+                    {
+                        if (x % 2 == 0 || y % 2 == 0)
+                        {
+                            continue;
+                        }
+                        if (y == _ROW_SIZE - 2)
+                        {
+                            if (x == _COLUMN_SIZE - 2)
+                            {
+                                continue;
+                            }
+                            _tile[y, x + 1] = TileType.Empty;
+                            continue;
+                        }
+                        if (x == _COLUMN_SIZE - 2)
+                        {
+                            _tile[y + 1, x] = TileType.Empty;
+                            continue;
+                        }
+                        if (rand.Next(2) == 0)
+                        {
+                            _tile[y, x + 1] = TileType.Empty;
+                            count++;    
+                        }
+                        else
+                        {
+                            int randomindex = rand.Next(0,count);
+                            _tile[y + 1, x - randomindex * 2] = TileType.Empty;
+                            count = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GenerateBySideWinderMyVersion()
+        {
+
+            {
+                List<int> intersectionCadinates = new List<int>();
+                int tempindex;
+                int coinSide;
+                //길을 다 막아버리는 작업
+                for (int y = 0; y < _ROW_SIZE; y++)
+                {
+                    for (int x = 0; x < _COLUMN_SIZE; x++)
+                    {
+                        if (x % 2 == 0 || y % 2 == 0)
+                        {
+                            _tile[y, x] = TileType.Wall;
+                        }
+                        else
+                        {
+                            _tile[y, x] = TileType.Empty;
+                        }
+                    }
+                }
+
+                //랜덤으로 우측 혹은 아래로 길을 둟는 작업
+                for (int y = 0; y < _ROW_SIZE; y++)
+                {
+                    for (int x = 0; x < _COLUMN_SIZE; x++)
+                    {
+
+                        if (x % 2 == 0 || y % 2 == 0)
+                        {
+                            continue;
+                        }
+                        if (y == _ROW_SIZE - 2)
+                        {
+                            if(x != _COLUMN_SIZE - 2)
+                            {
+                                _tile[y, x + 1] = TileType.Empty;
+                            }
+                            continue;
+                        }
+                        intersectionCadinates.Add(x);
+                        coinSide = rand.Next(2);
+                        if (coinSide == 0 && x != _COLUMN_SIZE - 2)
+                        {
+                            _tile[y, x + 1] = TileType.Empty;
+                            continue;
+                        }
+                        if (coinSide == 0 && x == _COLUMN_SIZE - 2)
+                        {
+                            tempindex = rand.Next(intersectionCadinates.Count);
+                            _tile[y + 1, intersectionCadinates[tempindex]] = TileType.Empty;
+                            intersectionCadinates.Clear();
+                            continue;
+                        }
+                        else //coinside==1
+                        {
+                            if (y == _ROW_SIZE - 2 && x == _COLUMN_SIZE-2)
+                            {
+                                continue;
+                            }
+                            if (y == _ROW_SIZE - 2)
+                            {
+                                _tile[y, x + 1] = TileType.Empty;
+                                continue;
+                            }
+                            tempindex = rand.Next(intersectionCadinates.Count);
+                            _tile[y + 1, intersectionCadinates[tempindex]] = TileType.Empty;
+                            intersectionCadinates.Clear();
+
+                        }
+                        //if (rand.Next(2) == 0)
+                        //{
+                        //    _tile[y, x + 1] = TileType.Empty;
+                        //}
+                        //else
+                        //{
+                        //    _tile[y + 1, x] = TileType.Empty;
+                        //}
+                    }
+                }
+            }
+        }
+
+        private void GenerateByBinaryTree()
+        {
+            //길을 다 막아버리는 작업
+            for (int y = 0; y < _ROW_SIZE; y++)
+            {
+                for (int x = 0; x < _COLUMN_SIZE; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
                     {
                         _tile[y, x] = TileType.Wall;
                     }
@@ -41,7 +195,43 @@ namespace Board
                 }
             }
 
+            //랜덤으로 우측 혹은 아래로 길을 둟는 작업
+            for (int y = 0; y < _ROW_SIZE; y++)
+            {
+                for (int x = 0; x < _COLUMN_SIZE; x++)
+                {
+
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        continue;
+                    }
+
+                    if (y == _ROW_SIZE - 2)
+                    {
+                        if (x == _COLUMN_SIZE - 2)
+                        {
+                            continue;
+                        }
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _COLUMN_SIZE - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+                    if (rand.Next(2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
         }
+
         public void Render()
         {
             ConsoleColor DefaultColor = Console.ForegroundColor;
